9b02db18b24c20663477b9492e62746ebbaeffa3
gergokutu <gergo.kutu@gmail.com>
2020-06-16 11:32:18 +0200

    Dispatch ADD/REPLACE to store

--- store.js
+++ store.js
@@ -1,10 +1,22 @@
 const redux = require("redux");
 const { reducer } = require("./reducer");
 const store = redux.createStore(reducer);
+console.log("Initial state of the store:", store.getState());
 
 // Runs the callbeck
 // When an action dispatched to the store
 store.subscribe(() => {
   console.log("Initial state of the store:", store.getState());
+})
+
+// Dispatch actions to the store
+store.dispatch({
+  type: "ADD",
+  payload: 42
+})
+
+store.dispatch({
+  type: "REPLACE",
+  payload: 5
 })
 
