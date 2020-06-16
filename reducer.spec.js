f7df2658b37008d40488f00951b9075557a9ef5c
gergokutu <gergo.kutu@gmail.com>
2020-06-16 13:00:14 +0200

    Implement 2 tests checking SQUARE action

--- reducer.spec.js
+++ reducer.spec.js
@@ -7,13 +7,16 @@ test("Initial state should be 0", () => {
 });
 
 test("ADD should work as expected", () => {
+  // Mock a redux store
   const store = redux.createStore(reducer);
 
+  // Dispatch an action
   store.dispatch({
     type: "ADD",
     payload: 1
   });
 
+  // Define the expected result
   expect(store.getState()).toBe(1);
 });
 
@@ -26,5 +29,27 @@ test("REPLACE should work as expected", () => {
   });
 
   expect(store.getState()).toBe(222);
+});
+
+test("SQUARE should work as expected", () => {
+  const store = redux.createStore(reducer);
+
+  store.dispatch({
+    type: "SQUARE",
+    payload: 3
+  });
+
+  expect(store.getState()).toBe(9);
+});
+
+test("SQUARE should work as expected", () => {
+  const store = redux.createStore(reducer);
+
+  store.dispatch({
+    type: "SQUARE",
+    payload: -10
+  });
+
+  expect(store.getState()).toBe(100);
 });
 
