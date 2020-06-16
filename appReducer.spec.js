7756191c31b60f34e2546594b17524f0b5e0a7d5
gergokutu <gergo.kutu@gmail.com>
2020-06-16 15:10:10 +0200

    Implement test for LOGIN

--- appReducer.spec.js
+++ appReducer.spec.js
@@ -4,4 +4,24 @@ const { reducer } = require("./appReducer");
 test("User initially is not logged in", () => {
   const store = redux.createStore(reducer);
   expect(store.getState().user).toBe(null);
-})
+});
+
+test("", () => {
+  const store = redux.createStore(reducer);
+
+  store.dispatch({
+    type: "LOGIN",
+    payload: {
+      firstName: "Heay",
+      lastName: "Joe",
+      age: 999
+    }
+  });
+
+  // ToEqual validates the structire of the object
+  expect(store.getState().user).toEqual({
+    firstName: "Heay",
+    lastName: "Joe",
+    age: 999
+  });
+});
