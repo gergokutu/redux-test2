1ed5f1740d91e2d91316046711fd4ae6a6f59144
gergokutu <gergo.kutu@gmail.com>
2020-06-16 15:18:38 +0200

    Test LOGOUT/UPDATE_LOGGEDIN_USER

--- appReducer.spec.js
+++ appReducer.spec.js
@@ -6,7 +6,7 @@ test("User initially is not logged in", () => {
   expect(store.getState().user).toBe(null);
 });
 
-test("", () => {
+test("User can log in", () => {
   const store = redux.createStore(reducer);
 
   store.dispatch({
@@ -23,5 +23,52 @@ test("", () => {
     firstName: "Heay",
     lastName: "Joe",
     age: 999
+  });
+});
+
+test("User can logout", () => {
+  const store = redux.createStore(reducer);
+
+  store.dispatch({
+    type: "LOGIN",
+    payload: {
+      firstName: "Heay",
+      lastName: "Joe",
+      age: 999
+    }
+  });
+
+  store.dispatch({
+    type: "LOGOUT"
+  });
+
+  expect(store.getState().user).toBe(null);
+});
+
+test("Update logged in user", () => {
+  const store = redux.createStore(reducer);
+
+  store.dispatch({
+    type: "LOGIN",
+    payload: {
+      firstName: "Heay",
+      lastName: "Joe",
+      age: 999
+    }
+  });
+
+  store.dispatch({
+    type: "UPDATE_LOGGEDIN_USER",
+    payload: {
+      age: 0,
+      pet: "cat"
+    }
+  });
+
+  expect(store.getState().user).toEqual({
+    firstName: "Heay",
+    lastName: "Joe",
+    age: 0,
+    pet: "cat"
   });
 });
